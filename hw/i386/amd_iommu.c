30b5c393d9a2eb99ee52dd9f678000ecdf47b889
Santosh Shukla <santosh.shukla@amd.com>
2024-03-19 07:35:48 -0500

    hw/pci: modify pci_setup_iommu() to set PCIIOMMUOps MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    This patch modifies pci_setup_iommu() to set PCIIOMMUOps
    instead of setting PCIIOMMUFunc. PCIIOMMUFunc is used to
    get an address space for a PCI device in vendor specific
    way. The PCIIOMMUOps still offers this functionality. But
    using PCIIOMMUOps leaves space to add more iommu related
    vendor specific operations.
    
    Cc: Kevin Tian <kevin.tian@intel.com>
    Cc: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Eric Auger <eric.auger@redhat.com>
    Cc: Yi Sun <yi.y.sun@linux.intel.com>
    Cc: David Gibson <david@gibson.dropbear.id.au>
    Cc: "Michael S. Tsirkin" <mst@redhat.com>
    Cc: Eric Auger <eric.auger@redhat.com>
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Andrey Smirnov <andrew.smirnov@gmail.com>
    Cc: Helge Deller <deller@gmx.de>
    Cc: Hervé Poussineau <hpoussin@reactos.org>
    Cc: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Cc: BALATON Zoltan <balaton@eik.bme.hu>
    Cc: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Cc: Jagannathan Raman <jag.raman@oracle.com>
    Cc: Matthew Rosato <mjrosato@linux.ibm.com>
    Cc: Eric Farman <farman@linux.ibm.com>
    Cc: Halil Pasic <pasic@linux.ibm.com>
    Cc: Christian Borntraeger <borntraeger@linux.ibm.com>
    Cc: Thomas Huth <thuth@redhat.com>
    Cc: Helge Deller <deller@gmx.de>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    [ clg: - refreshed on latest QEMU
           - included hw/remote/iommu.c
           - documentation update
           - asserts in pci_setup_iommu()
           - removed checks on iommu_bus->iommu_ops->get_address_space
           - included Elroy PCI host (PA-RISC) ]
    Signed-off-by: Cédric Le Goater <clg@redhat.com>
    
    [Santosh - Manually backported the patch to v4.2]
    
    Signed-off-by: Santosh Shukla <santosh.shukla@amd.com>

--- hw/i386/amd_iommu.c
+++ hw/i386/amd_iommu.c
@@ -1448,6 +1448,10 @@ static AddressSpace *amdvi_host_dma_iommu(PCIBus *bus, void *opaque, int devfn)
     return &iommu_as[devfn]->as;
 }
 
+static const PCIIOMMUOps amdvi_iommu_ops = {
+    .get_address_space = amdvi_host_dma_iommu,
+};
+
 static const MemoryRegionOps mmio_mem_ops = {
     .read = amdvi_mmio_read,
     .write = amdvi_mmio_write,
@@ -1570,7 +1574,7 @@ static void amdvi_sysbus_realize(DeviceState *dev, Error **errp)
                           AMDVI_MMIO_SIZE);
     memory_region_add_subregion(get_system_memory(), AMDVI_BASE_ADDR,
                                 &s->mmio);
-    pci_setup_iommu(bus, amdvi_host_dma_iommu, s);
+    pci_setup_iommu(bus, &amdvi_iommu_ops, s);
     msi_init(&s->pci.dev, 0, 1, true, false, errp);
     amdvi_init(s);
 }
