c4cdd1298b74853e0001843f212837b1cbe3619b
Shivank Garg <shivankg@amd.com>
2024-09-12 11:29:07 +0530

    hostmem: Refactor host mempolicy validation and maxnode calculation
    Introduce find_numa_maxnode() and verify_policy_hostnodes() to replace
    inline logic for handling the calculation of the NUMA maxnode and host
    mempolicy validation.
    
    Preparatory patch for adding NUMA policy support for KVM guest-memfd to
    avoid code duplication.
    
    No functional change.
    
    Signed-off-by: Shivank Garg <shivankg@amd.com>

--- backends/hostmem.c
+++ backends/hostmem.c
@@ -364,52 +364,32 @@ host_memory_backend_memory_complete(UserCreatable *uc, Error **errp)
         qemu_madvise(ptr, sz, QEMU_MADV_DONTDUMP);
     }
 #ifdef CONFIG_NUMA
-    unsigned long lastbit = find_last_bit(backend->host_nodes, MAX_NODES);
-    /* lastbit == MAX_NODES means maxnode = 0 */
-    unsigned long maxnode = (lastbit + 1) % (MAX_NODES + 1);
+    unsigned long maxnode = find_numa_maxnode(backend->host_nodes);
     /*
      * Ensure policy won't be ignored in case memory is preallocated
      * before mbind(). note: MPOL_MF_STRICT is ignored on hugepages so
      * this doesn't catch hugepage case.
      */
     unsigned flags = MPOL_MF_STRICT | MPOL_MF_MOVE;
-    int mode = backend->policy;
+    int policy = backend->policy;
 
-    /* check for invalid host-nodes and policies and give more verbose
-     * error messages than mbind(). */
-    if (maxnode && backend->policy == MPOL_DEFAULT) {
-        error_setg(errp, "host-nodes must be empty for policy default,"
-                   " or you should explicitly specify a policy other"
-                   " than default");
-        return;
-    } else if (maxnode == 0 && backend->policy != MPOL_DEFAULT) {
-        error_setg(errp, "host-nodes must be set for policy %s",
-                   HostMemPolicy_str(backend->policy));
+    if (!verify_policy_hostnodes(policy, sizeof(backend->host_nodes), maxnode,
+        errp)) {
         return;
     }
 
-    /*
-     * We can have up to MAX_NODES nodes, but we need to pass maxnode+1
-     * as argument to mbind() due to an old Linux bug (feature?) which
-     * cuts off the last specified node. This means backend->host_nodes
-     * must have MAX_NODES+1 bits available.
-     */
-    assert(sizeof(backend->host_nodes) >=
-           BITS_TO_LONGS(MAX_NODES + 1) * sizeof(unsigned long));
-    assert(maxnode <= MAX_NODES);
-
 #ifdef HAVE_NUMA_HAS_PREFERRED_MANY
-    if (mode == MPOL_PREFERRED && numa_has_preferred_many() > 0) {
+    if (policy == MPOL_PREFERRED && numa_has_preferred_many() > 0) {
         /*
          * Replace with MPOL_PREFERRED_MANY otherwise the mbind() below
          * silently picks the first node.
          */
-        mode = MPOL_PREFERRED_MANY;
+        policy = MPOL_PREFERRED_MANY;
     }
 #endif
 
     if (maxnode &&
-        mbind(ptr, sz, mode, backend->host_nodes, maxnode + 1, flags)) {
+        mbind(ptr, sz, policy, backend->host_nodes, maxnode + 1, flags)) {
         if (backend->policy != MPOL_DEFAULT || errno != ENOSYS) {
             error_setg_errno(errp, errno,
                              "cannot bind memory to host NUMA nodes");
